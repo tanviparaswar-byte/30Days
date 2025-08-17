7c1ab40a53091f20c0cd4da63e696de665825248
tanviparaswar-byte <tanviparaswar@gmail.com>
2025-08-17 20:05:32 +0530

    Create day3.cpp
    Day 3 of 30 Days challenge. To find duplicate number given in the array.

---
+++ day3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a[]={3,1,3,4,2};
+int n=5;
+int duplicatenum;
+for(int i=0;i<n;i++){
+for(int j=i+1;j<n;j++){
+if(a[i]==a[j]){
+duplicatenum=a[j];
+}
+}
+}
+cout<<"Duplicate number is:"<<duplicatenum
+return 0;
+}
